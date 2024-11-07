495d77705bc0bd7c4cc463519fc00cf4ffe23616
cnssky <135142716+cnssky@users.noreply.github.com>
2024-11-07 17:15:38 +0300

    uuuu
    gggggggg

---
+++ footer.php
@@ -0,0 +1,113 @@
+ <!-- footer begin -->
+        <footer class="footer-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-1">
+                        <div class="widget">
+                            <h5>Hizmetlerimiz</h5>
+                            <ul>
+                                <li><a href="#">İç Mekan Tasarımı</a></li>
+                                <li><a href="#">Kentsel Tasarım</a></li>
+                                <li><a href="#">3D Modelleme ve Görselleştirme</a></li>
+                                <li><a href="#">İnşaat ve Uygulama Danışmanlığı</a></li>
+                                <li><a href="#">Mimari Danışmanlık</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-1">
+                        <div class="widget">
+                            <h5>Kurumsal</h5>
+                            <ul>
+                                <li><a href="#">Hakkımızda</a></li>
+                                <li><a href="#">Basında Biz</a></li>
+                                <li><a href="#">Müşteri Hikayeleri</a></li>
+                                <li><a href="#">İletişim</a></li>
+                                <li><a href="#">Sıkça Sorulan Sorular (SSS)</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-1">
+                        <div class="widget">
+                            <h5>Etkinliklerimiz</h5>
+                            <ul>
+                                <li><a href="#">Kentsel Dönüşüm ve Sürdürülebilirlik Workshopları</a></li>
+                                <li><a href="#">Dijital Tasarım ve 3D Modelleme Workshopları</a></li>
+                                <li><a href="#">Mimari Aydınlatma Tasarımı Atölyesi</a></li>
+                                <li><a href="#">Tarihi Yapı Restorasyonu Workshopları</a></li>
+                                <li><a href="#">Bambu ve Ahşap Yapı Teknikleri Atölyesi</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-1">
+                        <div class="widget">
+                            <h5>Size Ulaşalım:</h5>
+                            <p>İletişim bilgilerinizi giriniz.</p>
+                            <form action="blank.php" class="row form-dark" id="form_subscribe" method="post" name="form_subscribe">
+                                <div class="col text-center">
+                                    <input class="form-control" id="txt_subscribe" name="txt_subscribe" placeholder="enter your email" type="text" /> <a href="#" id="btn-subscribe"><i class="arrow_right bg-color-secondary"></i></a>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </form>
+                            <div class="spacer-10"></div>
+                            <small>E-posta adresiniz bizimle güvende. Sizi rahatsız etmeyiz.</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="de-flex">
+                                <div class="de-flex-col">
+                                    <a href="index.html">
+                                        <img alt="logo-li" class="f-logo" src="images/logo-li.png" sizes="16x16"/><span class="copy">&copy; MİVA Mimarlık - Yaratıcı Tasarımlar, Kalıcı İzler.</span>
+                                    </a>
+                                 
+                                </div>
+                                <div class="de-flex-col">
+                                    <div class="social-icons">
+                                        <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+                                        <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                                        <a href="#"><i class="fa fa-linkedin fa-lg"></i></a>
+                                        <a href="#"><i class="fa fa-pinterest fa-lg"></i></a>
+                                        <a href="#"><i class="fa fa-rss fa-lg"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer close -->
+        
+    </div>
+
+    
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/OwlCarousel2Thumbs.min.js"></script>
+    <script src="js/validation.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/enquire.min.js"></script>
+    <script src="js/jquery.plugin.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script src="js/jquery.lazy.min.js"></script>
+    <script src="js/jquery.lazy.plugins.min.js"></script>
+    <script src="js/jquery.smartsticky.min.js"></script>
+    <script src="js/mdb.min.js"></script>
+    <script src="js/designesia.js"></script>
+
+
+</body>
+
+</html>
